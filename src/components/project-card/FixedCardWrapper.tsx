07789ec6809fee82b9351f6a4e6b8542ebaa1858
Deepesh Gupta <deepesh@findcoder.io>
2023-07-07 11:05:34 +0530

    homepage tough design complete

---
+++ src/components/project-card/FixedCardWrapper.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { PropsWithChildren, useRef } from "react";
+import { Container, ProjectCard } from "..";
+import { ProjectDocument } from "@/types";
+import Link from "next/link";
+
+type Props = PropsWithChildren<{
+    open: boolean;
+    top: string;
+    bottom: string;
+    left: string;
+    right: string;
+    width: string;
+    height: string;
+    project: ProjectDocument;
+}>;
+
+function FixedCardWrapper({ project, children, top, bottom, left, right, width, height, open }: Props) {
+    const card = useRef<HTMLDivElement>(null);
+
+    return (
+        <div
+            className={`duration-500 fixed z-40 ${!open ? "invisible" : ""}`}
+            ref={card}
+            style={{ top, bottom, left, right, width, height }}
+        >
+            <div className={`w-full duration-300 ${open ? "hidden" : ""}`}>
+                <ProjectCard project={project} />
+            </div>
+            {open && (
+                <div className="border border-white/20 w-full h-screen">
+                    <div className="overflow-auto w-full h-full bg-lightenDark py-12">
+                        <Container>
+                            <div className="max-w-2xl mx-auto">
+                                <div className="flex mb-4">
+                                    <Link href={"/"}>
+                                        <span className="inline-flex w-8 h-8 rounded-full border border-white/20 text-white/70 hover:text-white/90 hover:bg-white/10 justify-center items-center">
+                                            <span className="relative -top-[1px]">&lt;</span>
+                                        </span>
+                                    </Link>
+                                </div>
+                                {children}
+                            </div>
+                        </Container>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default FixedCardWrapper;
--- src/components/project-card/ProjectCardHome.tsx
+++ src/components/project-card/ProjectCardHome.tsx
@@ -1,88 +1,67 @@
 "use client";
 import { ProjectDocument } from "@/types";
 import { useParams } from "next/navigation";
-import React, { PropsWithChildren, useCallback, useEffect, useRef } from "react";
+import React, { PropsWithChildren, useCallback, useEffect, useRef, useState } from "react";
 import ProjectCard from "./ProjectCard";
-import { Container } from "..";
-import Link from "next/link";
+import FixedCardWrapper from "./FixedCardWrapper";
 
 type Props = PropsWithChildren<{
-    link?: boolean;
     project: ProjectDocument;
 }>;
 
-function ProjectCardHome({ project, children, link }: Props) {
+function ProjectCardHome({ project, children }: Props) {
     const { id } = useParams();
+    const [position, setPosition] = useState({
+        top: "",
+        bottom: "",
+        left: "",
+        right: "",
+        width: "",
+        height: "",
+    });
+
     const isActive = id === project.$id;
 
-    const card = useRef<HTMLDivElement>(null);
     const cardContainer = useRef<HTMLDivElement>(null);
 
     const setPositions = useCallback(() => {
-        if (card.current && cardContainer.current) {
+        if (cardContainer.current) {
             const containerRect = cardContainer.current.getBoundingClientRect();
 
-            if (isActive && card.current) {
-                card.current.style.top =
-                    card.current.style.left =
-                    card.current.style.right =
-                    card.current.style.bottom =
-                        `0px`;
-                card.current.style.width = `100%`;
-                card.current.style.height = `100%`;
+            if (isActive) {
+                setPosition(() => ({
+                    top: "0",
+                    bottom: "0",
+                    left: "0",
+                    right: "0",
+                    width: "100%",
+                    height: "100%",
+                }));
             } else {
-                card.current.style.top = `${containerRect.top}px`;
-                card.current.style.left = `${containerRect.left}px`;
-                card.current.style.width = `${containerRect.width}px`;
-                card.current.style.height = `${containerRect.height}px`;
-                card.current.style.right = card.current.style.bottom = `unset`;
+                setPosition(() => ({
+                    top: `${containerRect.top}px`,
+                    left: `${containerRect.left}px`,
+                    width: `${containerRect.width}px`,
+                    height: `${containerRect.height}px`,
+                    bottom: `unset`,
+                    right: `unset`,
+                }));
             }
         }
     }, [isActive]);
 
     useEffect(setPositions, [setPositions]);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (card.current) card.current.style.transition = "0ms";
-            setPositions();
-            if (card.current) card.current.style.transition = "300ms";
-        };
-
-        document.addEventListener("scroll", handleScroll);
-        return () => document.removeEventListener("scroll", handleScroll);
-    }, [setPositions]);
-
     return (
         <>
             <div className="w-full" ref={cardContainer}>
-                <div className="opacity-0">
-                    <ProjectCard project={project} link={false} />
+                <div className={isActive ? "opacity-0" : ""}>
+                    <ProjectCard project={project} />
                 </div>
             </div>
-            <div className={`duration-500 fixed ${isActive ? "z-40" : ""}`} ref={card}>
-                <div className={`w-full duration-300 ${isActive ? "hidden" : ""}`}>
-                    <ProjectCard project={project} link={link} />
-                </div>
-                {isActive && (
-                    <div className="border border-white/20 w-full h-screen">
-                        <div className="overflow-auto w-full h-full bg-lightenDark py-12">
-                            <Container>
-                                <div className="max-w-2xl mx-auto">
-                                    <div className="flex mb-4">
-                                        <Link href={"/"}>
-                                            <span className="inline-flex w-8 h-8 rounded-full border border-white/20 text-white/70 hover:text-white/90 hover:bg-white/10 justify-center items-center">
-                                                <span className="relative -top-[1px]">&lt;</span>
-                                            </span>
-                                        </Link>
-                                    </div>
-                                    {children}
-                                </div>
-                            </Container>
-                        </div>
-                    </div>
-                )}
-            </div>
+            <FixedCardWrapper {...position} open={id === project.$id} project={project}>
+                {children}
+            </FixedCardWrapper>
         </>
     );
 }
